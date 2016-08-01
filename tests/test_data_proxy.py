6e8f72e6aa9daf662b66da5029660c5a821cdfe2
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2016-08-02 00:55:00 +0200

    Add tests for DataProxy.update

--- tests/test_data_proxy.py
+++ tests/test_data_proxy.py
@@ -303,3 +303,74 @@ class TestDataProxy(BaseTest):
         assert d.get('normal') is missing
         assert d.get('loaded') == "foo"
         assert d.get('loaded_but_empty') == missing
+
+    def test_update(self):
+
+        class MySchema(EmbeddedSchema):
+            a = fields.IntField(required=True)
+            b = fields.IntField(missing=69)
+            c = fields.IntField(dump_only=True)
+            d = fields.IntField(attribute='e')
+
+        class MyOtherSchema(EmbeddedSchema):
+            a = fields.IntField()
+            b = fields.IntField()
+            c = fields.IntField()
+            d = fields.IntField(attribute='e')
+
+        d = DataProxy(MySchema(), {'a': 1, 'b': 2, 'd': 4})
+        d.set('c', 3)
+
+        # Update without reset_missings
+        # a and b are modified, c and d are unchanged
+        d.clear_modified()
+        d.update({'a': 11, 'b': 12})
+        assert d.get('a') == 11
+        assert d.get('b') == 12
+        assert d.get('c') == 3
+        assert d.get('d') == 4
+        assert set(['a', 'b']) == d._modified_data
+
+        # Update with reset_missings
+        # a is modified
+        # b is set to its default value
+        # c is not affected because it is dump_only
+        # d is set to missing
+        d.clear_modified()
+        d.update({'a': 21}, reset_missings=True)
+        assert d.get('a') == 21
+        assert d.get('b') == 69
+        assert d.get('c') == 3
+        assert d.get('d') == missing
+        assert set(['a', 'b', 'e']) == d._modified_data
+
+        # Update with required field missing
+        # Without reset_missings
+        d.clear_modified()
+        d.update({'d': 41})
+        assert d.get('a') == 21
+        assert d.get('b') == 69
+        assert d.get('c') == 3
+        assert d.get('d') == 41
+        assert set(['e']) == d._modified_data
+        # With reset_missings: exception raised
+        with pytest.raises(ValidationError):
+            d.update({'d': 41}, reset_missings=True)
+
+        # Update with Schema provided as parameter
+        # Without reset_missings
+        d.clear_modified()
+        d.update({'d': 42}, schema=MyOtherSchema())
+        assert d.get('a') == 21
+        assert d.get('b') == 69
+        assert d.get('c') == 3
+        assert d.get('d') == 42
+        assert set(['e']) == d._modified_data
+        # With reset_missings
+        d.clear_modified()
+        d.update({'d': 43}, schema=MyOtherSchema(), reset_missings=True)
+        assert d.get('a') == missing
+        assert d.get('b') == missing
+        assert d.get('c') == missing
+        assert d.get('d') == 43
+        assert set(['a', 'b', 'c', 'e']) == d._modified_data
