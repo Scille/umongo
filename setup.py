ee35be6683c5deb26cb4b9259e224d740a880b64
Thibault Lemaire <thibault.lemaire@davidson.fr>
2021-06-30 11:12:53 +0200

    Make package announce that it supports type hints

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     url='https://github.com/touilleMan/umongo',
     packages=['umongo', 'umongo.frameworks'],
     include_package_data=True,
+    package_data={'umongo': ["py.typed"]},
     python_requires='>=3.7',
     install_requires=requirements,
     extras_require={
