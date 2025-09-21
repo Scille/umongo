6f4dad1862c71eb1cf4263aceac2356267805184
Jérôme Lafréchoux <jerome@jolimont.fr>
2025-09-21 12:04:08 +0200

    Fix setup.py: python_requires 3.9

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     url='https://github.com/touilleMan/umongo',
     packages=['umongo', 'umongo.frameworks'],
     include_package_data=True,
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     install_requires=requirements,
     extras_require={
         'motor': ['motor>=3.1.1'],
